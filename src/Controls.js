eb1e68c5a80f5e79f4e70fad61024070f433595f
Matthew Mone <mmone@vernier.com>
2021-06-13 20:27:21 -0700

    mouse lock and strafe

--- src/Controls.js
+++ src/Controls.js
@@ -1,33 +1,80 @@
 export class Controls {
-  constructor() {
-    this.codes = { 37: 'left', 39: 'right', 38: 'forward', 40: 'backward' };
-    this.states = { left: false, right: false, forward: false, backward: false };
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.canvas.onclick = async () => {
+      document.addEventListener(
+        'pointerlockchange',
+        () => {
+          this.locked = true;
+          console.log('locked');
+          document.addEventListener(
+            'pointerlockchange',
+            () => {
+              this.locked = false;
+              console.log('unlocked');
+            },
+            { capture: false, once: true },
+          );
+        },
+        { capture: false, once: true },
+      );
+      await this.canvas.requestPointerLock();
+    };
+
+    this.codes = {
+      65: 'left',
+      37: 'left',
+      68: 'right',
+      39: 'right',
+      87: 'forward',
+      38: 'forward',
+      83: 'backward',
+      40: 'backward',
+    };
+    this.states = {
+      left: false,
+      right: false,
+      forward: false,
+      backward: false,
+      rotate: false,
+    };
     document.addEventListener('keydown', this.onKey.bind(this, true), false);
     document.addEventListener('keyup', this.onKey.bind(this, false), false);
     document.addEventListener('touchstart', this.onTouch.bind(this), false);
     document.addEventListener('touchmove', this.onTouch.bind(this), false);
     document.addEventListener('touchend', this.onTouchEnd.bind(this), false);
+    this.canvas.addEventListener('mousemove', this.onMouseMove.bind(this), false);
   }
 
-  onTouch(e) {
-    const t = e.touches[0];
-    this.onTouchEnd(e);
+  onTouch(event) {
+    const t = event.touches[0];
+    this.onTouchEnd(event);
     if (t.pageY < window.innerHeight * 0.5) this.onKey(true, { keyCode: 38 });
     else if (t.pageX < window.innerWidth * 0.5) this.onKey(true, { keyCode: 37 });
     else if (t.pageY > window.innerWidth * 0.5) this.onKey(true, { keyCode: 39 });
   }
 
-  onTouchEnd(e) {
+  onTouchEnd(event) {
     this.states = { left: false, right: false, forward: false, backward: false };
-    e.preventDefault();
-    e.stopPropagation();
+    if (event.preventDefault) event.preventDefault();
+    if (event.stopPropagation) event.stopPropagation();
   }
 
-  onKey(val, e) {
-    const state = this.codes[e.keyCode];
+  onMouseMove({ movementX }) {
+    if (!this.locked) return;
+    const negative = movementX < 0 ? -1 : 1;
+    this.states.rotate = negative * (Math.min(Math.abs(movementX), 25) * 0.3);
+    requestAnimationFrame(() => {
+      this.states.rotate = false;
+    });
+  }
+
+  onKey(val, event) {
+    if (!this.locked) return;
+    const state = this.codes[event.keyCode];
     if (typeof state === 'undefined') return;
     this.states[state] = val;
-    e.preventDefault && e.preventDefault();
-    e.stopPropagation && e.stopPropagation();
+    if (event.preventDefault) event.preventDefault();
+    if (event.stopPropagation) event.stopPropagation();
   }
 }
--- src/Player.js
+++ src/Player.js
@@ -10,8 +10,12 @@ export class Player {
     this.paces = 0;
   }
 
+  get perpendicular() {
+    return this.direction + Math.PI / 2;
+  }
+
   rotate(angle) {
-    this.direction = (this.direction + angle + CIRCLE) % CIRCLE;
+    this.direction = (this.direction + (angle * Math.PI) / 180 + CIRCLE) % CIRCLE;
   }
 
   walk(distance, map) {
@@ -22,10 +26,19 @@ export class Player {
     this.paces += distance;
   }
 
+  strafe(distance, map) {
+    const dx = Math.cos(this.perpendicular) * distance;
+    const dy = Math.sin(this.perpendicular) * distance;
+    if (map.get(this.x + dx, this.y) <= 0) this.x += dx;
+    if (map.get(this.x, this.y + dy) <= 0) this.y += dy;
+    this.paces += distance;
+  }
+
   update(controls, map, seconds) {
-    if (controls.left) this.rotate(-Math.PI * seconds);
-    if (controls.right) this.rotate(Math.PI * seconds);
+    if (controls.left) this.strafe(-3 * seconds, map);
+    if (controls.right) this.strafe(3 * seconds, map);
     if (controls.forward) this.walk(3 * seconds, map);
     if (controls.backward) this.walk(-3 * seconds, map);
+    if (controls.rotate) this.rotate(controls.rotate);
   }
 }
