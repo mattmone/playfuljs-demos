bdb71eb4e4e1d73ea60d910e963341bf14c503fa
Matthew Mone <mmone@vernier.com>
2021-06-13 20:27:43 -0700

    controls from main

--- src/main.js
+++ src/main.js
@@ -6,24 +6,21 @@ import { Camera } from './Camera.js';
 import { GameLoop } from './GameLoop.js';
 import { MOBILE } from './constants.js';
 
-const display = document.getElementById('gameCanvas');
-const ctx = display.getContext('2d');
-const width = (display.width = window.innerWidth);
-const height = (display.height = window.innerHeight);
+const gameCanvas = document.getElementById('gameCanvas');
 
-const terrain = new Terrain(9);
-terrain.generate(0.7);
-terrain.draw(ctx, width, height);
+gameCanvas.width = window.innerWidth;
+gameCanvas.height = window.innerHeight;
+console.log(window.innerHeight);
 
 const player = new Player(15.3, -1.2, Math.PI * 0.3);
 const map = new Map(32);
-const controls = new Controls();
-const camera = new Camera(display, MOBILE ? 160 : 320, 0.8);
+const controls = new Controls(gameCanvas);
+const camera = new Camera(gameCanvas, MOBILE ? 160 : 320, 0.8);
 const loop = new GameLoop();
 
 map.randomize();
 
-loop.start(function frame(seconds) {
+loop.start(seconds => {
   map.update(seconds);
   player.update(controls.states, map, seconds);
   camera.render(player, map);
