d579d19999cd5e0cb7e5e9645c1a9f02307912da
Matthew Mone <mmone@vernier.com>
2021-06-18 10:28:03 -0700

    constants

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,20 @@
 export const CIRCLE = Math.PI * 2;
 export const MOBILE = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent);
+
+export const TEXTURE = {
+  WALL: 1,
+  STAIRS: {
+    IN: {
+      NORTH: 2,
+      EAST: 3,
+      SOUTH: 4,
+      WEST: 5,
+    },
+    OUT: {
+      NORTH: 6,
+      EAST: 7,
+      SOUTH: 8,
+      WEST: 9,
+    },
+  },
+};
