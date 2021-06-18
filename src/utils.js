7836f57f9b6976f019541fedf184f801a1dfde70
Matthew Mone <mmone@vernier.com>
2021-06-18 10:14:30 -0700

    add utility functions

---
+++ src/utils.js
@@ -0,0 +1,18 @@
+export function oneOf(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+export function removeFromArray(arr, value) {
+  arr.splice(arr.indexOf(value), 1);
+  return arr;
+}
+
+export function rollDice(sides = 2, times = 1) {
+  let total = 0;
+  while (times > 0) {
+    total += Math.ceil(Math.random() * sides);
+    times--;
+  }
+  if (!total) return 0;
+  return total;
+}
