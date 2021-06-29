a66b1e99a72356786fd3fd42317d3791e2b627e4
Matthew Mone <mmone@vernier.com>
2021-06-28 18:54:54 -0700

    clean doubleraf

--- src/main.js
+++ src/main.js
@@ -7,41 +7,40 @@ import { MOBILE, EFFECTS } from './constants.js';
 
 const game = document.querySelector('#game');
 const welcome = document.querySelector('#welcome-screen');
+const doubleRaf = () =>
+  new Promise(resolve => requestAnimationFrame(() => requestAnimationFrame(() => resolve())));
 
 document.querySelector('#enter').addEventListener('click', async () => {
   welcome.toggleAttribute('hidden', true);
   game.removeAttribute('hidden');
   if (MOBILE) await game.requestFullscreen();
-  requestAnimationFrame(() => {
-    requestAnimationFrame(() => {
-      const gameCanvas = document.getElementById('gameCanvas');
+  await doubleRaf();
+  const gameCanvas = document.getElementById('gameCanvas');
 
-      gameCanvas.width = game.innerWidth;
-      gameCanvas.height = game.innerHeight;
+  gameCanvas.width = game.innerWidth;
+  gameCanvas.height = game.innerHeight;
 
-      let map = new Map(32);
-      const player = new Player(map.startingPosition);
-      const controls = new Controls(gameCanvas);
-      const camera = new Camera(gameCanvas, MOBILE ? 320 : 640, 0.8);
-      const loop = new GameLoop();
+  let map = new Map(32);
+  const player = new Player(map.startingPosition);
+  const controls = new Controls(gameCanvas);
+  const camera = new Camera(gameCanvas, MOBILE ? 320 : 640, 0.8);
+  const loop = new GameLoop();
 
-      player.addEventListener('player-use', () => {
-        if (map.near('exit', player)) {
-          map = new Map(32);
-          player.setNewMap(map.startingPosition);
-          player.addEffect({ effect: EFFECTS.TELEPORT, duration: 2000 });
-        }
-      });
-      player.addEventListener('player-position-change', () => {
-        const nearby = map.nearBy(player);
-        player.notify(nearby);
-      });
+  player.addEventListener('player-use', () => {
+    if (map.near('exit', player)) {
+      map = new Map(32);
+      player.setNewMap(map.startingPosition);
+      player.addEffect({ effect: EFFECTS.TELEPORT, duration: 2000 });
+    }
+  });
+  player.addEventListener('player-position-change', () => {
+    const nearby = map.nearBy(player);
+    player.notify(nearby);
+  });
 
-      loop.start(seconds => {
-        map.update(seconds);
-        player.update(controls.states, map, seconds);
-        camera.render(player, map);
-      });
-    });
+  loop.start(seconds => {
+    map.update(seconds);
+    player.update(controls.states, map, seconds);
+    camera.render(player, map);
   });
 });
